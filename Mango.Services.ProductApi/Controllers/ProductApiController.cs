59fe75d5bb7c7d9486e37fa1863b0ea5e2953743
Tonye Roberts <talentedrex@gmail.com>
2022-08-18 15:37:00 +0100

    Add get endpoints to products api

---
+++ Mango.Services.ProductApi/Controllers/ProductApiController.cs
@@ -0,0 +1,56 @@
+﻿using Mango.Services.ProductApi.Models.Dto;
+using Mango.Services.ProductApi.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductApi.Controllers
+{
+    [Route("api/products")]
+    public class ProductApiController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IProductRepository _productRepository;
+
+        public ProductApiController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            this._response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+                _response.Result = productDtos;
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                ProductDto productDto = await _productRepository.GetProductById(id);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+
+    }
+}
