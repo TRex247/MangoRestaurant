2b6c4beed6c58f7c9dee232b1a4ba19711b0756d
Tonye Roberts <talentedrex@gmail.com>
2022-09-02 16:27:47 +0100

    Add login partial to Mango.Web

--- Mango.Services.Identity/SD.cs
+++ Mango.Services.Identity/SD.cs
@@ -39,8 +39,8 @@ namespace Mango.Services.Identity
                     ClientId = "mango",
                     ClientSecrets = { new Secret("secret".Sha256())},
                     AllowedGrantTypes = GrantTypes.Code,
-                    RedirectUris = { "https://localhost:44383/sigin-oidc" },
-                    PostLogoutRedirectUris = { "https://localhost:44383/signout-callback-oidc" },
+                    RedirectUris = { "https://localhost:7252/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:7252/signout-callback-oidc" },
                     AllowedScopes = new List<string>
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
