82f9e26f5d83db050c04935d1ba6fc99bf7dfe9d
Tonye Roberts <talentedrex@gmail.com>
2022-09-13 12:25:37 +0100

    Add feature to register user from UI

--- Mango.Services.Identity/MainModule/Account/AccountController.cs
+++ Mango.Services.Identity/MainModule/Account/AccountController.cs
@@ -240,7 +240,7 @@ namespace IdentityServerHost.Quickstart.UI
                     Email = model.Email,
                     EmailConfirmed = true,
                     FirstName = model.FirstName,
-                    LastName = model.LastNAme
+                    LastName = model.LastName
                 };
 
                 var result = await _userManager.CreateAsync(user, model.Password);
@@ -262,7 +262,7 @@ namespace IdentityServerHost.Quickstart.UI
                     await _userManager.AddClaimsAsync(user, new Claim[]{
                             new Claim(JwtClaimTypes.Name, model.Username),
                             new Claim(JwtClaimTypes.Email, model.Email),
-                            new Claim(JwtClaimTypes.FamilyName, model.LastNAme),
+                            new Claim(JwtClaimTypes.FamilyName, model.LastName),
                             new Claim(JwtClaimTypes.GivenName, model.FirstName),
                             new Claim(JwtClaimTypes.WebSite, "http://"+model.Username+".com"),
                             new Claim(JwtClaimTypes.Role,"User") });
@@ -306,6 +306,11 @@ namespace IdentityServerHost.Quickstart.UI
                 }
             }
 
+            // Populate RoleName select list
+            List<string> roles = new List<string>();
+            roles.Add("Admin");
+            roles.Add("Customer");
+            ViewBag.message = roles;
             // If we got this far, something failed, redisplay form
             return View(model);
         }
