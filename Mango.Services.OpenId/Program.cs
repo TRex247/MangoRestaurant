7974a8078a2eefb1fe17395493b8242fe3f78ac9
Tonye Roberts <talentedrex@gmail.com>
2022-09-23 00:34:26 +0100

    Add DB contest and configure OpenId server.

--- Mango.Services.OpenId/Program.cs
+++ Mango.Services.OpenId/Program.cs
@@ -1,8 +1,117 @@
+using Mango.Services.OpenId.Data;
+using Mango.Services.OpenId.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Quartz;
+using System.Configuration;
+using static OpenIddict.Abstractions.OpenIddictConstants;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    // Configure the context to use Microsoft SQL Server.
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+    // Register the entity sets needed by OpenIddict.
+    // Note: use the generic overload if you need
+    // to replace the default OpenIddict entities.
+    options.UseOpenIddict();
+});
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders()
+            .AddDefaultUI();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Configure Identity to use the same JWT claims as OpenIddict instead
+    // of the legacy WS-Federation claims it uses by default (ClaimTypes),
+    // which saves you from doing the mapping in your authorization controller.
+    options.ClaimsIdentity.UserNameClaimType = Claims.Name;
+    options.ClaimsIdentity.UserIdClaimType = Claims.Subject;
+    options.ClaimsIdentity.RoleClaimType = Claims.Role;
+    options.ClaimsIdentity.EmailClaimType = Claims.Email;
+
+    // Note: to require account confirmation before login,
+    // register an email sender service (IEmailSender) and
+    // set options.SignIn.RequireConfirmedAccount to true.
+    //
+    // For more information, visit https://aka.ms/aspaccountconf.
+    options.SignIn.RequireConfirmedAccount = false;
+});
+
+// OpenIddict offers native integration with Quartz.NET to perform scheduled tasks
+// (like pruning orphaned authorizations/tokens from the database) at regular intervals.
+builder.Services.AddQuartz(options =>
+{
+    options.UseMicrosoftDependencyInjectionJobFactory();
+    options.UseSimpleTypeLoader();
+    options.UseInMemoryStore();
+});
+
+// Register the Quartz.NET service and configure it to block shutdown until jobs are complete.
+builder.Services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
+
+builder.Services.AddOpenIddict()
+
+    // Register the OpenIddict core components.
+    .AddCore(options =>
+    {
+        // Configure OpenIddict to use the Entity Framework Core stores and models.
+        // Note: call ReplaceDefaultEntities() to replace the default OpenIddict entities.
+        options.UseEntityFrameworkCore()
+        .UseDbContext<ApplicationDbContext>();
+
+        // Enable Quartz.NET integration.
+        options.UseQuartz();
+    })
+
+    // Register the OpenIddict server components.
+    .AddServer(options =>
+    {
+        // Enable the authorization, logout, token and userinfo endpoints.
+        options.SetAuthorizationEndpointUris("/connect/authorize")
+        .SetLogoutEndpointUris("/connect/logout")
+        .SetTokenEndpointUris("/connect/token")
+        .SetUserinfoEndpointUris("/connect/userinfo");
+
+        // Mark the "email", "profile" and "roles" scopes as supported scopes.
+        options.RegisterScopes(Scopes.Email, Scopes.Profile, Scopes.Roles);
+
+        // Note: this sample only uses the authorization code flow but you can enable
+        // the other flows if you need to support implicit, password or client credentials.
+        options.AllowAuthorizationCodeFlow();
+
+        // Register the signing and encryption credentials.
+        options.AddDevelopmentEncryptionCertificate()
+        .AddDevelopmentSigningCertificate();
+
+        // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+        options.UseAspNetCore()
+        .EnableAuthorizationEndpointPassthrough()
+        .EnableLogoutEndpointPassthrough()
+        .EnableTokenEndpointPassthrough()
+        .EnableUserinfoEndpointPassthrough()
+        .EnableStatusCodePagesIntegration();
+    })
+
+    // Register the OpenIddict validation components.
+    .AddValidation(options =>
+    {
+        // Import the configuration from the local OpenIddict server instance.
+        options.UseLocalServer();
+
+        // Register the ASP.NET Core host.
+        options.UseAspNetCore();
+    });
+
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
         .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
