750acd22e08a74dfe8f52fbd665963e37754f43a
Tonye Roberts <talentedrex@gmail.com>
2022-10-25 21:40:30 +0100

    Add entity models for shopping cart

---
+++ Mango.Services.ShoppingCartApi/Models/CartDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mango.Services.ShoppingCartApi.Models
+{
+    public class CartDetails
+    {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+
+        [ForeignKey("CartHeader")]
+        public virtual CartHeader CartHeader { get; set; }
+        [ForeignKey("Product")]
+        public virtual Product Product { get; set; }
+    }
+}
