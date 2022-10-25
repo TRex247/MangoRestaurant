8db1c946160c3d5599d3da7b87777fb9e1cc6b19
Tonye Roberts <talentedrex@gmail.com>
2022-10-25 21:42:15 +0100

    Update CartDetails model

--- Mango.Services.ShoppingCartApi/Models/CartDetails.cs
+++ Mango.Services.ShoppingCartApi/Models/CartDetails.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Mango.Services.ShoppingCartApi.Models
 {
     public class CartDetails
     {
+        [Key]
         public int CartDetailsId { get; set; }
         public int CartHeaderId { get; set; }
         public int ProductId { get; set; }
