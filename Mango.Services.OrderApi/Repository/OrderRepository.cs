34f97a5c7b6d34aa72a30d97610eab938006f493
Tonye Roberts <talentedrex@gmail.com>
2022-12-13 14:46:45 +0100

    Add order repository interface

---
+++ Mango.Services.OrderApi/Repository/OrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using Mango.Services.OrderApi.Models;
+
+namespace Mango.Services.OrderApi.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public Task<bool> AddOrder(OrderHeader orderHeader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateOrderPaymentStatus(int OrderHeaderId, bool paid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
